f93420cf3e06c938b353b5ee95eef37198c632d8
maricmara <camara.mariana@escolar.ifrn.edu.br>
2025-02-07 10:10:47 -0300

    passo2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Tabuleiro meuTabuleiro = new Tabuleiro();
+        
+        meuTabuleiro.MostrarTabuleiro();
+        
+        Console.WriteLine("Movendo a peça da posição (1,0) para (3,0):");
+        
+        if (meuTabuleiro.MoverPeca(1, 0, 3, 0))
+            Console.WriteLine("Movimento realizado com sucesso!");
+        
+        meuTabuleiro.MostrarTabuleiro();
+    }
+}
